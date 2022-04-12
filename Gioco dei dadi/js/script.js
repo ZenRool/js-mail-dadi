027efed5efeae844a20097a9c2300d200435aa61
ZenRool <lorenzopapperini@hotmail.it>
2022-04-12 15:33:27 +0200

    Lancio del dado con controllo del vincitore

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,4 +1,21 @@
 // Gioco dei dadi
+// lancio del dado 
 function getRandomDice() {
     return Math.floor(Math.random() * 6 +1);
+}
+// lancio del utente 
+const userDice = getRandomDice();
+// lancio del computer 
+const cpuDice = getRandomDice();
+
+// console.log ("user ",userDice);
+// console.log("cpu ", cpuDice);
+if (userDice === cpuDice) {
+    alert ("Parità ", cpuDice , userDice);
+}
+else if (userDice > cpuDice) {
+    alert ("Vince l'utente " , userDice , cpuDice);
+}
+else {
+    alert ("Vince il computer " , cpuDice , userDice);
 }
